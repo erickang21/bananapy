ae22369339b23ec5c047ed767b96590149c78c08
bananaboy21 <kang.eric.hi@gmail.com>
2018-11-11 09:37:42 -0500

    set up some funcs etc

--- bananapy/client.py
+++ bananapy/client.py
@@ -9,4 +9,33 @@ class Error(Exception):
 
 
 class Client:
-    
+    """
+    Main Client of BananAPI.
+
+    Params:
+    token (str): The BananAPI token.
+    """
+    def __init__(self, token, session=None):
+        self.session = session or aiohttp.ClientSession()
+        self.token = token
+        self.base_url = "https://bananapi.ml/api/"
+
+    async def _get(self, endpoint):
+        """
+        Private function to request from the API.
+
+        This should not be called directly.
+        """
+        res = await self.session.get(self.base_url + endpoint)
+        if res.status != 200:
+            try:
+                resp = await res.json()
+                msg = resp.get("message", "No message found.")
+            except:
+                msg = await res.text()
+            raise Error("{}: {}".format(res.status, resp))
+        else:
+            return res
+
+
+
